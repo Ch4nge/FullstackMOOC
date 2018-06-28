24e2b3ecc396975065e5d15f4c31ff0710534265
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-06-28 11:31:38 +0300

    Modify folder names

--- 1/unicafe/src/Statistic.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Statistic = (props) => {
-    return(
-      <tr><td>{props.statistic.text}</td><td>{props.statistic.number}</td></tr>
-    )
-  }
-export default Statistic
